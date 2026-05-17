0cdff32183176b336764901c31bdccbbb563e7c6
Kannam Charitha <vtu25568@veltech.edu.in>
2026-05-17 12:08:07 +0530

    Add files via upload

---
+++ test_connection.php
@@ -0,0 +1,5 @@
+<?php
+include 'config.php';
+echo "Database connection successful!";
+?></content>
+<parameter name="filePath">c:\Users\vtu25\OneDrive\Desktop\jobManagement\Placement_portal_management\test_connection.php
